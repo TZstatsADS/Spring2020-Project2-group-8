a5a30bf3a19809fc847a69da8e5c3c710a5c8533
SHYYHS-123 <60301281+SHYYHS-123@users.noreply.github.com>
2020-02-22 00:13:07 -0500

    Add files via upload

---
+++ hanbojiao/test/global.test.1.R
@@ -0,0 +1,63 @@
+
+packages.used=c("DT","tidyverse", "shiny","shinythemes","shinythemes","datasets","shinyWidgets")
+packages.needed=setdiff(packages.used,
+                        intersect(installed.packages()[,1],
+                                  packages.used))
+
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+library(tidyverse)
+library(shiny)
+# library(leaflet)
+# library(data.table)
+# library(plotly)
+library(shinythemes)
+library(DT)
+
+#Statistics Analysis Global Enviroment 
+
+#Loading the required data:
+
+load("data_comparison.RData")
+
+data_comparison<-
+  data_comparison%>%mutate(Calories_percent=percent_rank(Calories)%>%round(2),
+                         Total_Fat_percent=percent_rank(Total_Fat)%>%round(2),
+                         Saturated_Fat_percent=percent_rank(Saturated_Fat)%>%round(2),
+                         Trans_Fat_percent=percent_rank(Trans_Fat)%>%round(2),
+                         Cholesterol_percent=percent_rank(Cholesterol)%>%round(2),
+                         Sodium_percent=percent_rank(Sodium)%>%round(2),
+                         Carbohydrates_percent=percent_rank(Carbohydrates)%>%round(2),
+                         Protein_percent=percent_rank(Protein)%>%round(2),
+                         Sugar_percent=percent_rank(Sugar)%>%round(2),
+                         Dietary_Fiber_percent=percent_rank(Dietary_Fiber)%>%round(2))
+
+nutrition<-c("Calories"="Calories_percent", "Total_Fat" ="Total_Fat_percent", "Saturated_Fat"="Saturated_Fat_percent", "Trans_Fat" ="Trans_Fat_percent", "Cholesterol"="Cholesterol_percent","Sodium"="Sodium_percent","Carbohydrates" ="Carbohydrates_percent","Protein" ="Protein_percent", "Sugar"="Sugar_percent","Dietary_Fiber"="Dietary_Fiber_percent" )
+
+nutrition<-c("Select",nutrition)
+
+# 
+# column(12,
+#        column(4,
+#               conditionalPanel('input.arrange1 != "NA"',
+#                                checkboxInput("desc1",
+#                                              label = "lower",
+#                                              value = F)
+#               )
+#        ),
+#        column(4,
+#               conditionalPanel('input.arrange2 != "NA"', 
+#                                checkboxInput("desc2",
+#                                              label = "lower",
+#                                              value = F)
+#               )
+#        ),
+#        column(4,
+#               conditionalPanel('input.arrange3 != "NA"', 
+#                                checkboxInput("desc3",
+#                                              label = "lower",
+#                                              value = F)
+#               )
+#        )
+# )
---
+++ hanbojiao/test/ui.test.1.R
@@ -0,0 +1,120 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(tidyverse)
+library(shiny)
+# library(leaflet)
+# library(data.table)
+# library(plotly)
+library(shinythemes)
+library(shinyWidgets)
+
+
+fluidPage(
+ shinyUI(
+    div(id="canvas",
+        navbarPage(strong("what to eat",style="color: white;"),
+                   theme=shinytheme("cerulean"),
+                   #theme = "bootstrap.min.css",
+                   #theme="styles.css
+                   tabPanel("Intro",
+                            column(2,
+                                column(12,
+                                       selectizeInput("restaurants",
+                                                   h4("choose some restaurants:"),
+                                                   as.list(data_comparison$restaurant%>%unique()),
+                                                   multiple = T, options = list(minItems=1, maxItems=3))
+                                       ),
+                                column(12,
+                                       conditionalPanel('input.restaurants != ""', 
+                                                        checkboxGroupInput("category_check", 
+                                                                           label = h4("Food Category"), 
+                                                                           choices = as.list(food_category),
+                                                                           selected = as.list(food_category),
+                                                                           inline = F)
+                                                        )
+                                       ),
+                                conditionalPanel('input.restaurants != ""',
+                                                 selectInput("arrange1",
+                                                             label = h4("choose first nuitrition:"),
+                                                             choice=nutrition),
+                                                 conditionalPanel('input.arrange1 != "NA"',
+                                                                  checkboxInput("desc1",
+                                                                                label = "lower",
+                                                                                value = F)
+                                                                  )
+                                                 ),
+                                conditionalPanel('input.arrange1 != "NA"',
+                                                 selectInput("arrange2",
+                                                             h4("choose second nuitrition:"),
+                                                             choice=nutrition),
+                                                 conditionalPanel('input.arrange2 != "NA"',
+                                                                  checkboxInput("desc2",
+                                                                                label = "lower",
+                                                                                value = F)
+                                                                  )
+                                                 ),
+                                conditionalPanel('input.arrange2 != "NA"',
+                                                 selectInput("arrange3",
+                                                             h4("choose third nuitrition:"),
+                                                             choice=nutrition),
+                                                 conditionalPanel('input.arrange3 != "NA"',
+                                                                  checkboxInput("desc3",
+                                                                                label = "lower",
+                                                                                value = F)
+                                                                  )
+                                                 )
+                                ),
+                            column(10,
+                                   conditionalPanel('input.restaurants.length>0 &&(input.arrange1 != "NA"||input.arrange2 != "NA"||input.arrange3 != "NA")',
+                                                    column(10,checkboxGroupInput("nutrition_show", 
+                                                                                label = "Columns in table to show", 
+                                                                                choices = as.list(nutrition[-1]%>%names()),
+                                                                                selected = as.list(nutrition[-1]%>%names()),
+                                                                                inline = T)),
+                                                    column(2,numericInput("topn", label = "Number of menu", value = 5))
+                                                    ),
+                                   conditionalPanel('input.restaurants.length>0 &&(input.arrange1 != "NA"||input.arrange2 != "NA"||input.arrange3 != "NA")',
+                                                    column(12,
+                                                           textOutput('res1_name'),
+                                                           dataTableOutput ('res1_table')
+                                                           ),conditionalPanel('input.res1_table_rows_selected!=""',
+                                                                              column(9, 
+                                                                                     plotOutput ('res1_plot')
+                                                                                     )
+                                                                              )
+                                                    ),
+                                   conditionalPanel('input.restaurants.length>1 &&(input.arrange1 != "NA"||input.arrange2 != "NA"||input.arrange3 != "NA")', 
+                                                    column(12,
+                                                           textOutput('res2_name'),
+                                                           dataTableOutput ('res2_table')
+                                                           ),conditionalPanel('input.res2_table_rows_selected!=""',
+                                                                              column(9, 
+                                                                                     plotOutput ('res2_plot')
+                                                                                     )
+                                                                              )
+                                                    ),
+                                   conditionalPanel('input.restaurants.length>2 &&(input.arrange1 != "NA"||input.arrange2 != "NA"||input.arrange3 != "NA")', 
+                                                    column(12,
+                                                           textOutput('res3_name'),
+                                                           dataTableOutput ('res3_table')
+                                                           ),conditionalPanel('input.res3_table_rows_selected!=""',
+                                                                              column(9, 
+                                                                                     plotOutput ('res3_plot')
+                                                                                     )
+                                                                              )
+                                                    )
+                                   )
+                            ),
+                   tabPanel("Intro"
+                            )
+                   )
+        )
+    )   
+)
